b97c6e3807978ab85148740fe758fef8361e20e3
Lorenzo <lorenzo@whirlwindsoftware.com>
2020-12-31 14:47:05 -0700

    new methods

--- MazeMaker/mazeFunctions.cs
+++ MazeMaker/mazeFunctions.cs
@@ -14,46 +14,29 @@ namespace MazeMaker
         }
 
         public static bool[,] startMaze(bool[,] maze, List<int[]> startWorm, int size, Random random, ref int blocksFilled)
-        {
-            string mazeStr = "";
-
-            //bool[,] maze = new bool[64, 64];
-            int[] startPoint = new int[2] { 0, 0 };
-            int[] exit = new int[2] { 0, 0 };
-
-            startPoint[0] = random.Next(64);
-            startPoint[1] = random.Next(64);
-
-            exit[0] = random.Next(64);
-            exit[1] = random.Next(64);
-
-            //List<int[]> startWorm = new List<int[]>();
-            List<int[]> endWormWorm = new List<int[]>();
-
+        {                        
+            int[] startPoint = new int[2] { random.Next(64), random.Next(64) };
+            maze[startPoint[0], startPoint[1]] = true;
+            startWorm.Add(startPoint);
+            
             int loops = 0;
-            while (startWorm.Count < size & loops < 4000)
+
+            while (startWorm.Count < size & loops < 8000)
             {
-                blocksFilled = startWorm.Count;
+                blocksFilled = startWorm.Count;//use to report to the outside world my progress
+                
                 int[] newTile = nextTile(startPoint, maze, startWorm, random);
-
-                if (newTile[0] != startPoint[0] | newTile[1] != startPoint[1])
+                
+                if (!startWorm.Exists(x => x[0] == newTile[0] & x[1] == newTile[1]))
                 {
-                    if (newTile[0] == 1 | newTile[0] == 63)
+                    if (newTile[0] != startPoint[0] ^ newTile[1] != startPoint[1]) //XOR
                     {
-                        newTile[0] = random.Next(63);
-                    }
-                    if (newTile[1] == 1 | newTile[1] == 63)
-                    {
-                        newTile[1] = random.Next(63);
-                    }
-
-                    if (!startWorm.Exists(x => x[0] == newTile[0] & x[1] == newTile[1]))
-                    {
-                        startPoint[0] = newTile[0];
+                        startPoint[0] = newTile[0];//update the new tile
                         startPoint[1] = newTile[1];
                         startWorm.Add(newTile);
                     }
                 }
+                
                 loops += 1;
             }
 
@@ -61,24 +44,115 @@ namespace MazeMaker
             {
                 maze = startMaze(maze, startWorm, size, random, ref blocksFilled);
             }
-
-
-            return maze;
-
-            //Console.WriteLine(maze);
+            return maze;            
         }
 
         public static int[] nextTile(int[] currentTile, bool[,] maze, List<int[]> startWorm, Random random)
         {
+            //int[] nextTile = new int[2];
+            int[] tempTile = new int[2];
+
+            currentTile.CopyTo(tempTile, 0);                        
+            //nextTile.CopyTo(tempTile, 0);
+
+            int direction = random.Next(4);
+            bool upDown = false;
+            if (direction == 0) //up
+            {
+                upDown = true;
+                tempTile[0] = currentTile[0] - 1;                
+            }
+            else if (direction == 1) //down
+            {
+                upDown = true;
+                tempTile[0] = currentTile[0] + 1;                
+            }
+            else if (direction == 2) //left
+            {
+                tempTile[1] = currentTile[1] - 1;                
+            }
+            else if (direction == 3) //right
+            {
+                tempTile[1] = currentTile[1] + 1;                
+            }
+            //check boundaries
+
+            if (tempTile[0] < 0 | tempTile[0] > 63 | tempTile[1] < 0 | tempTile[1] > 63)
+            {
+                currentTile.CopyTo(tempTile, 0);
+                return tempTile;
+            }
+
+            if (!maze[tempTile[0], tempTile[1]])//is it already been change?
+            {
+                if (upDown)
+                {
+                    if (checkLeftRight(tempTile, maze))
+                    {
+                        maze[tempTile[0], tempTile[1]] = true;
+                        return tempTile;
+                    }
+                }
+                else
+                {
+                    if (checkUpDown(tempTile, maze))
+                    { 
+                        maze[tempTile[0], tempTile[1]] = true;
+                        return tempTile;
+                    }
+                }                                
+            }
+            currentTile.CopyTo(tempTile, 0);
+            return tempTile;            
+        }
+
+
+        public static bool checkLeftRight(int[] tile, bool[,] maze)
+        {
+            bool leftEmpty = false;
+            bool rightEmpty = false;
+
+            if (tile[1] + 1 <64)
+            {
+                rightEmpty = !maze[tile[0], tile[1] + 1];
+            }
+
+            if (tile[1] - 1 > 0)
+            {
+                leftEmpty = !maze[tile[0], tile[1] - 1];
+            }
+
+            return rightEmpty & leftEmpty;
+                        
+        }
+        public static bool checkUpDown(int[] tile, bool[,] maze)
+        {
+            bool downEmpty = false;
+            bool upEmpty = false;
+
+            if (tile[0] + 1 < 64)
+            {
+                downEmpty = !maze[tile[0] + 1, tile[1]];
+            }
+
+            if (tile[0] - 1 > 0)
+            {
+                upEmpty = !maze[tile[0] -1, tile[1]];
+            }
+
+            return downEmpty & upEmpty;            
+        }
+
+
+        public static int[] nextTileOld(int[] currentTile, bool[,] maze, List<int[]> startWorm, Random random)
+        {
             int[] nextTile = new int[2];
             currentTile.CopyTo(nextTile, 0);
+
             int[] tempTile = new int[2];
             nextTile.CopyTo(tempTile, 0);
 
-
             int direction = random.Next(4);
-
-
 
             if (direction == 0) //up
             {
@@ -174,47 +248,9 @@ namespace MazeMaker
                 }
             }
             tempTile = startWorm[random.Next(startWorm.Count)];
-            //tempTile[0] = random.Next(64);
-            //tempTile[1] = random.Next(64);
+
             return tempTile;
         }
-
-
-        public static bool checkLeftRight(int[] tile, bool[,] maze)
-        {
-            try
-            {
-                if (!maze[tile[0], tile[1] + 1] & !maze[tile[0], tile[1] - 1])
-                {
-                    return true;
-                }
-            }
-            catch (Exception err)
-            {
-                return false;
-                //throw;
-            }
-
-
-            return false;
-        }
-        public static bool checkUpDown(int[] tile, bool[,] maze)
-        {
-            try
-            {
-                if (!maze[tile[0] + 1, tile[1]] & !maze[tile[0], tile[1] - 1])
-                {
-                    return true;
-                }
-            }
-            catch (Exception err)
-            {
-                return false;
-                //throw;
-            }
-            return false;
-        }
-
         public static string mazeToString(bool[,] maze)
         {
             string mazeStr = "";
@@ -224,11 +260,12 @@ namespace MazeMaker
                 {
                     if (maze[i, j])
                     {
-                        mazeStr += "6603";
+                        mazeStr += "2600"; //low ground
                     }
                     else
                     {
-                        mazeStr += "4400";
+                        
+                        mazeStr += "6603";
                     }
                 }
             }
