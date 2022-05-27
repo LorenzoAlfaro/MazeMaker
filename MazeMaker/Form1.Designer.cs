94e0cf8354180a1c4f7eed674b90aac53b511d01
Lorenzo Alfaro <lorenzoac88@gmail.com>
2022-05-26 19:17:33 -0600

    style: clean up the form layout
    closes #1

--- MazeMaker/Form1.Designer.cs
+++ MazeMaker/Form1.Designer.cs
@@ -41,32 +41,35 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 463);
+            this.button5.Location = new System.Drawing.Point(12, 166);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(122, 27);
+            this.button5.Size = new System.Drawing.Size(146, 34);
             this.button5.TabIndex = 1;
-            this.button5.Text = "Load Binary";
+            this.button5.Text = "Load Binary Viewer";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(330, 463);
+            this.button6.Location = new System.Drawing.Point(12, 246);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(178, 47);
+            this.button6.Size = new System.Drawing.Size(146, 34);
             this.button6.TabIndex = 2;
             this.button6.Text = "Write Map";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(453, 399);
+            this.label9.Location = new System.Drawing.Point(260, 147);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(48, 13);
             this.label9.TabIndex = 3;
@@ -75,7 +78,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(454, 356);
+            this.label1.Location = new System.Drawing.Point(165, 147);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 4;
@@ -84,7 +87,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(330, 512);
+            this.checkBox1.Location = new System.Drawing.Point(168, 126);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(102, 17);
             this.checkBox1.TabIndex = 5;
@@ -93,71 +96,86 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(154, 463);
+            this.button7.Location = new System.Drawing.Point(12, 126);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(149, 34);
+            this.button7.Size = new System.Drawing.Size(146, 34);
             this.button7.TabIndex = 6;
-            this.button7.Text = "Move Location";
+            this.button7.Text = "Create new Map";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(154, 388);
+            this.textBox1.Location = new System.Drawing.Point(12, 378);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "MRGN";
+            this.textBox1.Visible = false;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(154, 356);
+            this.textBox2.Location = new System.Drawing.Point(12, 326);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
             this.textBox2.TabIndex = 8;
-            this.textBox2.Text = "offset";
+            this.textBox2.Text = "Offset";
+            this.textBox2.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(330, 379);
+            this.button1.Location = new System.Drawing.Point(12, 286);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.Size = new System.Drawing.Size(146, 34);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Search Label";
+            this.button1.Text = "Search Label inside binary";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(15, 393);
+            this.textBox3.Location = new System.Drawing.Point(12, 352);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.Size = new System.Drawing.Size(146, 20);
             this.textBox3.TabIndex = 10;
             this.textBox3.Text = "SectionSize";
+            this.textBox3.Visible = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(30, 526);
+            this.button2.Location = new System.Drawing.Point(12, 206);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 24);
+            this.button2.Size = new System.Drawing.Size(146, 34);
             this.button2.TabIndex = 11;
-            this.button2.Text = "readUnits";
+            this.button2.Text = "Find Units inside binary file";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(33, 200);
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(204, 95);
+            this.listBox1.Size = new System.Drawing.Size(146, 95);
             this.listBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Files inside original map";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 570);
+            this.ClientSize = new System.Drawing.Size(342, 463);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
@@ -193,6 +211,7 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
