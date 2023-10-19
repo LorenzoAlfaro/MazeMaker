67b6583ac204f07a6f0c65984c19fc45748a535a
Lorenzo Alfaro <lorenzoac88@gmail.com>
2023-10-19 15:44:45 -0600

    feat: add function to export file
    Complications of using DllImport and marsheling values correctly.

--- MazeMaker/Wrapper.cs
+++ MazeMaker/Wrapper.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace MazeMaker
 {
     public static class WrapperMpq
-    {        
-        
+    {
+
         //string mazePath = Environment.ExpandEnvironmentVariables(@"C:\Users\%USERNAME%\Dropbox\VIDEO GAMES\SC MAPS\MPQExport\Maze.scm");
         //string extractPath = Environment.ExpandEnvironmentVariables(@"C:\Users\%USERNAME%\Dropbox\VIDEO GAMES\SC MAPS\MPQExport");
-        
 
         [DllImport("Called.dll", CharSet = CharSet.Unicode)]
         public static extern string FunctionCalled([MarshalAs(UnmanagedType.BStr)] string input); //LPWstr return first three letters
@@ -26,8 +26,7 @@ namespace MazeMaker
         {
             int DefaultMaxFiles = 1024;
             int DefaultBlockSize = 3;
-            
-            
+
             int hMPQ = SFmpq.MpqOpenArchiveForUpdateEx(FileName, SFmpq.MOAU_OPEN_EXISTING | SFmpq.MOAU_MAINTAIN_LISTFILE, DefaultMaxFiles, DefaultBlockSize);
 
             if (hMPQ == 0 || hMPQ == SFmpq.INVALID_HANDLE_VALUE)
@@ -45,15 +44,14 @@ namespace MazeMaker
 
         public static int DeleteFile(string MPQPath, string FileName)
         {
-            //bool close = SFmpq.SFileCloseArchive(hMPQ);            
 
             int hMPQ = mOpenMPQ(MPQPath);
             SFmpq.SFileSetLocale(0);
 
             bool success = SFmpq.MpqDeleteFile(hMPQ, FileName);
 
-            return SFmpq.MpqCloseUpdatedArchive(hMPQ, 0);            
-                                
+            return SFmpq.MpqCloseUpdatedArchive(hMPQ, 0);
+
         }
 
         public static void ListFiles(string Path, int hMPQ)
@@ -61,6 +59,61 @@ namespace MazeMaker
             SFmpq.SFileOpenArchive(Path, 0, 0, ref hMPQ);
 
 
+        }
+
+        public static bool ExportFile(string MPQPath, string extractedFile, string saveFilePath)
+        {
+            int fNum;
+            int result;
+            int hMPQ = 0;
+
+            if(!SFmpq.SFileOpenArchive(MPQPath, 0, 0, ref hMPQ))
+            {
+                return false;
+            }
+
+            sGetFile(hMPQ, extractedFile, saveFilePath, 1);
+
+            SFmpq.SFileCloseArchive(hMPQ);
+
+            return true;
+        }
+
+        public static void sGetFile(int hMPQ, string FileName, string OutPath, int UseFullPath)
+        {
+            byte[] buffer = new byte[3];
+            int hFile=0;
+            long fLen=0;
+            int cNum=0;
+            dynamic overLapped = 0;
+
+            if (SFmpq.SFileOpenFileEx(hMPQ, FileName, 0, ref hFile))
+            {
+                fLen = SFmpq.SFileGetFileSize(hFile, 0);
+
+                if(fLen > 0)
+                {
+                   Array.Resize(ref buffer, (int)fLen);
+                }
+                else
+                {
+                    Array.Resize(ref buffer, 0);
+                }
+
+                SFmpq.SFileReadFile(hFile, buffer, (int)fLen, 0, ref overLapped);
+                SFmpq.SFileCloseFile(hFile);
+
+                if(UseFullPath == 0)
+                {
+                    FileName = Path.GetFileName(FileName);
+                }
+
+                FileName = $@"{OutPath}\{FileName}";
+
+
+                File.WriteAllBytes(FileName, buffer);
+
+            }
         }
 
         //List.ListItems.Item(fNum).ListSubItems(4).Tag = 0
@@ -90,6 +143,6 @@ namespace MazeMaker
         //List<FILELISTENTRY> ListedFiles = new List<FILELISTENTRY>();
 
 
-        //bool close = Classes.SFmpq.SFileCloseArchive(hMPQ); //call when finish the file    
+        //bool close = Classes.SFmpq.SFileCloseArchive(hMPQ); //call when finish the file
     }
 }
