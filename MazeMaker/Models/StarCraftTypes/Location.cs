0eed769a8705122dd8e6b1b9a681df57b8448961
Lorenzo Alfaro <lorenzoac88@gmail.com>
2023-10-26 17:11:17 -0600

    refactor: separate classes into their own files

---
+++ MazeMaker/Models/StarCraftTypes/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MazeMaker
+{
+    public class Location
+    {
+        public int leftX; //u32
+        public int topY; //u32
+        public int rightX; //u32
+        public int bottY;
+        public Int16 stringNumber; //u16
+        public Int16 flags; //u16
+        public long offSet;
+        // Location is 20 bytes
+        // 4+4+4+4+2+2=20
+    }
+}
---
+++ MazeMaker/Models/StarCraftTypes/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MazeMaker
+{
+    public class Unit
+    {
+        public int instanceNum;
+        public Int16 x;//center of the unit
+        public Int16 y;
+        public Int16 ID;
+        public Int16 AddonNydus;
+        public Int16 Properties;
+        public Int16 mapProperties;
+        public Byte PlayerNumber;
+        public Byte HP;
+        public Byte Shield;
+        public Byte Energy;
+        public int ResourceAmount;
+        public Int16 unitHangar;
+        public Int16 unitFlags;
+        public int unused;
+        public int AddonNydusLink;
+        public long offSet;
+    }
+}
